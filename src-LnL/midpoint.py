43ff3e78cd486f58afea2f28cd8c38d39b7583b7
Yong-Hoon Kim <yong.hoon.kim@kaist.ac.kr>
2019-03-12 18:14:21 +0900

    Add Linge & Langtangen source code depository

---
+++ src-LnL/midpoint.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def midpoint(f, a, b, n):
+    h = float(b-a)/n
+    result = 0
+    for i in range(n):
+        result += f((a + h/2.0) + i*h)
+    result *= h
+    return result
---
+++ src-LnL/trapezoidal.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def trapezoidal(f, a, b, n):
+    h = float(b-a)/n
+    result = 0.5*f(a) + 0.5*f(b)
+    for i in range(1, n):
+        result += f(a + i*h)
+    result *= h
+    return result
+    
