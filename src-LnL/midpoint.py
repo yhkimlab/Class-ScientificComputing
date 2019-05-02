248290c28f9e65d7e69b91852a768996fc6537df
starnmj <starnmj@tgm-master.hpc>
2019-05-02 15:05:45 +0900

    file sorting

--- src-LnL/midpoint.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def midpoint(f, a, b, n):
-    h = float(b-a)/n
-    result = 0
-    for i in range(n):
-        result += f((a + h/2.0) + i*h)
-    result *= h
-    return result
